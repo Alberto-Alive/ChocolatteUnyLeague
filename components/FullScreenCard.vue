4cdace29097e922edca486b5ae28730c0ead292b
Alive <albertopopescu@outlook.com>
2023-03-24 03:54:30 +0000

    change background image

--- components/FullScreenCard.vue
+++ components/FullScreenCard.vue
@@ -4,7 +4,7 @@
     <div class="h-[95vh] bg-[#efefef] bg-opacity-60 backdrop-blur-sm p-4 rounded-xl shadow-lg">
         <slot />
         <img
-            src="/img/logored.png"
+            src="/img/logo.png"
             alt="Signature"
             class="flex absolute h-16 bottom-[-20px] right-0 md:right-[-30px] -rotate-12 pointer-events-none sign"
         />

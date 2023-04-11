e72e79c58a9473da1ba98f4feaa431e562e7e577
Alive <albertopopescu@outlook.com>
2023-04-11 23:40:58 +0100

    add email and phone

--- pages/donation.vue
+++ pages/donation.vue
@@ -2,13 +2,9 @@
 useHead({
     title: 'Donation',
 });
-
 const provider = ref([
-    { name: 'Sociabuzz', url: 'https://sociabuzz.com/chocolatte' },
-    { name: 'Trakteer', url: 'https://trakteer.id/chocolattech_/tip' },
-    { name: 'Saweria', url: 'https://saweria.co/ChocoLatte' },
-    { name: 'Ko-fi', url: 'https://ko-fi.com/chocolattech' },
-    { name: 'Streamlabs', url: 'https://streamlabs.com/chocolattechannel/tip' },
+    { details: 'unyleague@outlook.com', url: 'mailto:unyleague@outlook.com', icon: 'mail', show: false },
+    { details: '+44 7780 790155', url: 'whatsapp://send?phone=+447780790155', icon: 'smartphone', show: false },
 ]);
 </script>
 
@@ -19,21 +15,17 @@ const provider = ref([
             <p class="text-base text-chocolate-700">
                 You can support by donating Chocoin to help choco creating more content!
             </p>
-            <div class="lg:grid grid-cols-1 lg:gap-2 md:grid-cols-3 mt-4">
-                <a
-                    v-for="prov in provider"
-                    :href="prov.url"
-                    class="button w-full my-1"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <Icon
-                        name="material-symbols:open-in-new"
-                        class="block flex-grow-0 shrink-0"
-                        width="32"
-                        height="32"
-                    />
-                    <span class="flex-grow flex-shrink text-left w-full">{{ prov.name }}</span>
+            <div class="lg:grid lg:gap-2 mt-4">
+                <a v-for="prov in provider" :href="prov.url" class="button w-50 my-1 relative" target="_blank"
+                    rel="noreferrer" @mouseenter="prov.show = true" @mouseleave="prov.show = false">
+                    <Icon :name="'material-symbols:' + prov.icon" class="block flex-grow-0 shrink-0" width="32"
+                        height="32" />
+                    <span v-if="prov.show" class="w-full opacity-0 hover:opacity-100 transition-opacity duration-300 z-10">
+                        {{ prov.details }}
+                    </span>
+                    <span v-else class="w-full">
+                        {{ 'HII' }}
+                    </span>
                 </a>
             </div>
             <p class="text-base text-chocolate-700 my-3">Each support is appreciated. Thanks for your support!</p>
